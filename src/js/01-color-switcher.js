cb90b41b31a94a840abff61d01b182afa54b9175
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-18 22:06:51 +0200

    task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.setAttribute('disabled', 'true');
+  document.body.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    startBtn.removeAttribute('disabled', 'true');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
